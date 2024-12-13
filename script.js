4f1f583eed80eaff25f3a9622c3f515914e6bbba
zuuw <codymcleod02@gmail.com>
2024-12-12 23:18:56 -0700

    changes of my finalproject

---
+++ script.js
@@ -0,0 +1,93 @@
+
+
+
+first.addEventListener('click',function()
+{
+    console.log('forst');
+    document.getElementById('assignment').src='first.png';
+    document.getElementById('linc').href='https://github.com/zuuw/cprg218-grid-flexbox';
+    let first = document.getElementById('first').style.backgroundColor = '#646464';
+    document.getElementById('second').style.backgroundColor = '#f1f1f1';
+    document.getElementById('third').style.backgroundColor = '#f1f1f1';
+    document.getElementById('forth').style.backgroundColor = '#f1f1f1';
+    document.getElementById('fifth').style.backgroundColor = '#f1f1f1';
+    document.getElementById('sixth').style.backgroundColor = '#f1f1f1';
+   
+    output.innerHTML = 'This is the first thing I did in your class this semester'
+
+});
+
+second.addEventListener('click',function()
+{
+    console.log('2');
+    document.getElementById('assignment').src='second.png';
+    document.getElementById('linc').href='https://github.com/zuuw/cprg-218-responsive-design';
+    let second = document.getElementById('second').style.backgroundColor = '#646464';
+    document.getElementById('first').style.backgroundColor = '#f1f1f1';
+    document.getElementById('third').style.backgroundColor = '#f1f1f1';
+    document.getElementById('forth').style.backgroundColor = '#f1f1f1';
+    document.getElementById('fifth').style.backgroundColor = '#f1f1f1';
+    document.getElementById('sixth').style.backgroundColor = '#f1f1f1';
+   
+    output.innerHTML = 'Responsive Design ft. Nobody'
+});
+
+third.addEventListener('click',function()
+{
+    console.log('3');
+    document.getElementById('assignment').src='third.png';
+    document.getElementById('linc').href='https://github.com/zuuw/cprg218-assignment1';
+    let third = document.getElementById('third').style.backgroundColor = '#646464';
+    document.getElementById('second').style.backgroundColor = '#f1f1f1';
+    document.getElementById('first').style.backgroundColor = '#f1f1f1';
+    document.getElementById('forth').style.backgroundColor = '#f1f1f1';
+    document.getElementById('fifth').style.backgroundColor = '#f1f1f1';
+    document.getElementById('sixth').style.backgroundColor = '#f1f1f1';
+   
+    output.innerHTML = 'Assignment 1 - Puffin'
+});
+
+forth.addEventListener('click',function()
+{
+    console.log('4');
+    document.getElementById('assignment').src='frth.png';
+    document.getElementById('linc').href='https://github.com/zuuw/cprg-218-javascript-intro';
+    let forth = document.getElementById('forth').style.backgroundColor = '#646464';
+    document.getElementById('second').style.backgroundColor = '#f1f1f1';
+    document.getElementById('third').style.backgroundColor = '#f1f1f1';
+    document.getElementById('first').style.backgroundColor = '#f1f1f1';
+    document.getElementById('fifth').style.backgroundColor = '#f1f1f1';
+    document.getElementById('sixth').style.backgroundColor = '#f1f1f1';
+   
+    output.innerHTML = 'How to turn on and off a light with JS!'
+});
+
+fifth.addEventListener('click',function()
+{
+    console.log('5');
+    document.getElementById('assignment').src='fifth.png';
+    document.getElementById('linc').href='https://github.com/zuuw/cprg-218-lab2';
+    let fifth = document.getElementById('fifth').style.backgroundColor = '#646464';
+    document.getElementById('second').style.backgroundColor = '#f1f1f1';
+    document.getElementById('third').style.backgroundColor = '#f1f1f1';
+    document.getElementById('forth').style.backgroundColor = '#f1f1f1';
+    document.getElementById('first').style.backgroundColor = '#f1f1f1';
+    document.getElementById('sixth').style.backgroundColor = '#f1f1f1';
+   
+    output.innerHTML = 'Buttons with JS!'
+});
+
+sixth.addEventListener('click',function()
+{
+    console.log('six');
+    document.getElementById('assignment').src='sixth.png';
+    document.getElementById('linc').href='https://github.com/zuuw/cprg-218-assignment-2';
+    let sixth = document.getElementById('sixth').style.backgroundColor = '#646464';
+    document.getElementById('second').style.backgroundColor = '#f1f1f1';
+    document.getElementById('third').style.backgroundColor = '#f1f1f1';
+    document.getElementById('forth').style.backgroundColor = '#f1f1f1';
+    document.getElementById('fifth').style.backgroundColor = '#f1f1f1';
+    document.getElementById('first').style.backgroundColor = '#f1f1f1';
+   
+    output.innerHTML = 'Assignment 2 - My timeline'
+});
